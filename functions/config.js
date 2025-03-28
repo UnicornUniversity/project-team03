7e697b90b5a11524d32a5631f43e3167c1a025f6
PavlaSp <pavlasplichalova@seznam.cz>
2025-03-28 11:46:51 +0100

    update

--- functions/config.js
+++ functions/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT || 5000,
+    port: process.env.APP_PORT || 5000,
     dbUri: process.env.DB_URI || 'mongodb://localhost:27017/malina'
 };
