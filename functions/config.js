a87ff0bb6f8e9d22b8546cdf68db3b45e0ee5e6c
Zuzka (Niko) <z.kordiakova@gmail.com>
2025-06-04 20:16:43 +0200

    none -> null fix

--- functions/config.js
+++ functions/config.js
@@ -6,5 +6,5 @@ const functions = require('firebase-functions'); // Pro přístup k Firebase env
 module.exports = {
     port: process.env.APP_PORT || 8080, // Port pro lokální vývoj
     dbUri: process.env.MONGODB_URI || 'mongodb://localhost:27017/malina', // Připojovací řetězec MongoDB
-    svcAcc: process.env.SERVICE_ACC || None,
+    svcAcc: process.env.SERVICE_ACC || null,
 };
